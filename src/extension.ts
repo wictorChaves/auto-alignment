27ea1fd63a5ca8dd1bcc38d8e74510da2f7850c8
Wictor Chaves <wictor@hotmail.com.br>
2019-07-06 23:50:13 -0300

    Correção de bugs

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
+'use strict';
 import * as vscode from 'vscode';
 import StartUp from './start-up';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerTextEditorCommand("wictor.autoalignment", editor => new StartUp(editor)));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand("wictor.autoalignment", (editor) => new StartUp(editor)));
 }
-
-export function deactivate() {}
+export function deactivate() { }
